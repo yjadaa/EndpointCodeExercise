42e94da3a0950d44635680f76aef103c64c21874
Yazan Jadaa <yjadaa@gmail.com>
2014-10-02 15:39:52 -0700

    Update myCtrl.js

--- Testing/myCtrl.js
+++ Testing/myCtrl.js
@@ -139,29 +139,3 @@ app.controller("listFilesDirResponseCtrl", function($scope,$http) {
         .success(function(response) {$scope.jsonData = response;})
     };
 });
-
-app.controller('TodoController', function($scope) {
-   $scope.filteredTodos = []
-  ,$scope.currentPage = 1
-  ,$scope.numPerPage = 10
-  ,$scope.maxSize = 5;
-  
-  $scope.makeTodos = function() {
-    $scope.todos = [];
-    for (i=1;i<=1000;i++) {
-      $scope.todos.push({ text:'todo '+i, done:false});
-    }
-  };
-  $scope.makeTodos(); 
-  
-  $scope.numPages = function () {
-    return Math.ceil($scope.todos.length / $scope.numPerPage);
-  };
-  
-  $scope.$watch('currentPage + numPerPage', function() {
-    var begin = (($scope.currentPage - 1) * $scope.numPerPage)
-    , end = begin + $scope.numPerPage;
-    
-    $scope.filteredTodos = $scope.todos.slice(begin, end);
-  });
-});
